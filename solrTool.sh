e768777d37fc81a1f75aab96f25d10b82b0c6b74
Xu Long Zhe <942980741@qq.com>
2017-07-21 17:40:12 +0800

    Update solrTool.sh

--- solrTool.sh
+++ solrTool.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# solr检查/修复脚本
+# 注意：本脚本只适用于集群状态下，2备份的solr
 
 function log() {
-	echo "`date +"%Y-%m-%d %H:%M:%S"` - $1" >> /var/log/dblog/solrTool.log
+	echo "`now` - $1" >> /var/log/dblog/solrTool.log
 }
 
 function echoAndLog() {
@@ -15,25 +17,29 @@ function logAndExe() {
 }
 
 function now() {
-	date +%s
+	date +"%Y-%m-%d %H:%M:%S"
 }
 
+# 获取所有Core名称
 function getCores() {
 	ls -l /index | grep -v solr.xml | awk '{print $9}' | awk -F '_' '{print $1}' | sort -u
 }
 
+# 在集群所有机器上运行命令
 function remote(){
 	for i in `cat /bigdata/salut/components/hadoop/etc/hadoop/slaves`;do
 		ssh $i $1
 	done
 }
 
+# 同remote(),不过输出结果中会包含IP地址
 function remoteWithIp(){
 	for i in `cat /bigdata/salut/components/hadoop/etc/hadoop/slaves`;do
 		ssh $i $1 | awk '{print "'"$i"'""    "$0}'
 	done
 }
 
+# 上传Solr配置文件
 function uploadConf() {
 	for core in `getCores`;do
 		len=${#core};
@@ -44,6 +50,7 @@ function uploadConf() {
 	done
 }
 
+# 清除Zookeeper上的Solr节点
 function clearSolrMeta() {
 	echo 'rmr /configs'           | zkCli.sh
 	echo 'rmr /overseer'          | zkCli.sh
@@ -53,14 +60,15 @@ function clearSolrMeta() {
 	echo 'rmr /clusterstate.json' | zkCli.sh
 }
 
-#output format
-# 1            2              3                                       4           5              6                                  7
-# replica1_IP  replica1_size  replica1_name                           replica2_IP replica2_size  replica2_name                      replica_size_diff
-# 21.60.1.91   40272         /index/FaceImage01_shard10_replica1      21.60.1.89  40708	        /index/FaceImage01_shard10_replica2 760
+# 检查replica大小，如果相差大于500M，会在最后一列标记 out-sync
+#
+# replica1_IP  replica1_size  replica1_name                           replica2_IP replica2_size  replica2_name                      replica_size_diff  
+# 21.60.1.91   812000         /index/FaceImage01_shard10_replica1      21.60.1.89  0	        /index/FaceImage01_shard10_replica2 812000                 out-sync
 function replicaCheck() {
 	remoteWithIp 'du /index --max-depth=1 | grep .*replica.*' | sort -k 3 -d | sed 'N;s/\n/\t/' | awk '{diff=sqrt(($2-$5)*($2-$5));str=$0"\t"$2-$5;if(diff>512000)str=str"  out-sync";print str}'
 }
 
+# 重新同步体积相差过大的Replica，可能会要求输入密码，如果没有配无密钥的话
 function replicaSync() {
 	for i in `replicaCheck | grep out-sync`;do
 		diff=`echo $i | 'awk {print $7}'`
@@ -78,6 +86,7 @@ function replicaSync() {
 	done
 }
 
+# 检查每个Collections状态是否正常，正常返回0，异常返回1
 function healthCheck() {
 	echoAndLog "Solr healthcheck..."
 	for core in `getCores`;do
@@ -90,10 +99,11 @@ function healthCheck() {
 			echoAndLog "$core: healthy"
 		fi
 	done
-	echoAndLog "Finish Checking,solr is healthy"
+	echoAndLog "Finish Checking, solr is healthy"
 	return 0
 }
 
+# 通过删除zk节点的方式修复solr
 function repair() {
 	echoAndLog "start to repair..."
 	remote 'dbserver.sh stop_server daemon'
@@ -106,6 +116,7 @@ function repair() {
 	echoAndLog 'Repair finish'
 }
 
+# 通过删除zk数据文件的方式修复solr
 function forceRepair() {
 	echoAndLog "start to force repair..."
 	dbserver.sh stop
@@ -117,11 +128,11 @@ function forceRepair() {
 	echoAndLog 'Force repair finish'
 }
 
+# 检查collection状态，如果不正常就执行修复
 function repairIfNeed() {
 	healthCheck
 	if [ "$?" != "0" ];then
 		repair
-		now > /var/log/dblog/lastRepair
 	else
 		echoAndLog "No need to repair"
 	fi
@@ -140,6 +151,10 @@ function printUsage() {
 	echo "	-syncReplica      copy biggest replica to another"
 }
 
+if [ "`ps -ef | grep $0 | grep -v $$ | grep -v grep`" ];then
+	echoAndLog "Already exists a instance of $0, exit"
+	exit 1
+fi
 cmd=$1
 if [ ! "$cmd" ];then
 	printUsage
